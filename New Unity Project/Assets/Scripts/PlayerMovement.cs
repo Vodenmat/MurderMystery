2ffbf2c570716c3925ae9942e1e8a2cb57a57e9b
Conner Anderson <21.conner.anderson@ksd.org>
2019-10-24 09:47:18 -0700

    Made lobby and hallway placeholders

---
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("r"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 velocity = new Vector2(x, y);
+        GetComponent<Rigidbody2D>().velocity = velocity * moveSpeed;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Stairs")
+        {
+            if (SceneManager.GetActiveScene().name == "Lobby")
+            {
+                SceneManager.LoadScene("Hallway");
+            }
+            else
+            {
+                SceneManager.LoadScene("Lobby");
+            }
+        }
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+}
