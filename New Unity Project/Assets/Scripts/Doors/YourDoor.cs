29df7fa7a654e5ace7f18a288802aa81f291eb78
Conner Anderson <21.conner.anderson@ksd.org>
2019-10-25 10:27:37 -0700

    Doors starting to work

---
+++ New Unity Project/Assets/Scripts/Doors/YourDoor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class YourDoor : MonoBehaviour
+{
+    public Text yourDoorText;
+    public Text Dialogue;
+    public GameObject enterButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        yourDoorText.text = "Your Room";
+
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        yourDoorText.text = "";
+    }
+}
