9c8aa27684c8739c77c056fe2ed9a96ece2e567b
Conner Anderson <21.conner.anderson@ksd.org>
2019-10-28 10:28:59 -0700

    Started on dialogue

---
+++ New Unity Project/Assets/Scripts/UI/Dialogue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Dialogue : MonoBehaviour
+{
+    public Text dialogueText;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OpeningText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void OpeningText()
+    {
+        dialogueText.text = "Thank you so much for coming detective!";
+        timer += Time.deltaTime;
+        if (timer > 5)
+        {
+            dialogueText.text = "The hotel hasn't been the same since the murder.";
+        }
+        if (this.name == "Manager")
+        {
+
+        }
+    }
+}
