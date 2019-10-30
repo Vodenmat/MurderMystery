3beaf2911a44538c8922bbbde8aa0ed050890ab8
Joshua Fielding <21.joshua.fielding@ksd.org>
2019-10-30 10:26:42 -0700

    rooms

---
+++ New Unity Project/Assets/Scripts/UI/PlayerTopDownMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTopDownMovement : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition.z = transform.position.z;
+        transform.right = mousePosition - transform.position;
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 moveDir = y * transform.right + x * transform.up;
+        moveDir.Normalize();
+        GetComponent<Rigidbody2D>().velocity = moveDir * moveSpeed;
+    }
+}
