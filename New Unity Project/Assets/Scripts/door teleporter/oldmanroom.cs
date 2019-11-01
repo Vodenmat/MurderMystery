0a7244be099b66118cf7e39f0e96853ef11b1eb9
Joshua Fielding <21.joshua.fielding@ksd.org>
2019-11-01 08:23:21 -0700

    celeb

---
+++ New Unity Project/Assets/Scripts/door teleporter/oldmanroom.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class oldmanroom : MonoBehaviour
+{
+    public int scoreCount = 0;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+
+    {
+        if (collision.gameObject.tag == "plus")
+        {
+            scoreCount++;
+            if (scoreCount >= 1)
+            {
+                SceneManager.LoadScene("Scene2");
+
+            }
+        }
+        else if (collision.gameObject.tag == "minus")
+        {
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
