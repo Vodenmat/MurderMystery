b05415f3db4bccabc5195f0913d1599699a671d3
Conner Anderson <connerwithahat@gmail.com>
2019-11-06 00:09:47 -0800

    Fadeout WIP

---
+++ New Unity Project/Assets/Scripts/UI/BlackOut.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BlackOut : MonoBehaviour
+{
+    public Color blackColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("Actions") == 0)
+        {
+            blackColor.a = 0;
+            blackColor.a += .01f;
+            GetComponent<Image>().color = blackColor;
+        }
+        else
+        {
+            blackColor.a = 1;
+            blackColor.a -= .01f;
+            GetComponent<Image>().color = blackColor;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
