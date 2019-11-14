a81a6bedfc7913f2033c4d307deb01592ca17ac7
Timothy Landers <21.timothy.landers@ksd.org>
2019-11-14 10:23:42 -0800

    EAsTERRR

--- New Unity Project/Assets/Scripts/Player/Actions.cs
+++ New Unity Project/Assets/Scripts/Player/Actions.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ActionCounter : MonoBehaviour
 {
+    public int health = 10;
+    public Text actionText;
+    public Slider actionSlider;
+    //public int lives;
     // Start is called before the first frame update
     void Start()
     {
-        
+        actionText.text = "HP: " + health;
+        actionSlider.maxValue = health;
+        actionSlider.value = health;
     }
 
     // Update is called once per frame
