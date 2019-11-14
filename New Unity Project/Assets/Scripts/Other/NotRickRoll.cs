087ca0e0984493a837f43ef15f358d0737b67b8f
Conner Anderson <21.conner.anderson@ksd.org>
2019-11-14 10:00:07 -0800

    Nothing suspicious

---
+++ New Unity Project/Assets/Scripts/Other/NotRickRoll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotRickRoll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "New Sprite")
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        }
+    }
+}
