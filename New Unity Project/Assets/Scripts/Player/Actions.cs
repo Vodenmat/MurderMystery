2456ce01a60af9911642ba53330d61855348f379
Timothy Landers <21.timothy.landers@ksd.org>
2019-11-15 09:06:14 -0800

    Action Counter

--- New Unity Project/Assets/Scripts/Player/Actions.cs
+++ New Unity Project/Assets/Scripts/Player/Actions.cs
@@ -2,23 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class ActionCounter : MonoBehaviour
+public class Actions : MonoBehaviour
 {
-    public int health = 10;
+    public int actions = 10;
     public Text actionText;
-    public Slider actionSlider;
-    //public int lives;
+
     // Start is called before the first frame update
     void Start()
     {
-        actionText.text = "HP: " + health;
-        actionSlider.maxValue = health;
-        actionSlider.value = health;
+        actionText.text = "Actions: " + (PlayerPrefs.GetInt("Actions")-1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        actionText.text = "Actions: " + (PlayerPrefs.GetInt("Actions")-1);
     }
 }
