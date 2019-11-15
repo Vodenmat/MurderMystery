d04230d31c746c3709dd07f80e175e4358d7cca3
Timothy Landers <21.timothy.landers@ksd.org>
2019-11-15 10:10:59 -0800

    nani,nyoom,alia,sans

---
+++ New Unity Project/Assets/Scripts/Other/Eggs4All.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eggs4All : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "New Sprite (2)")
+        {
+            if (this.gameObject.name == "Nyoom")
+            {
+                System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=Gbn7F0wXqQc");
+            }
+            else
+            {
+                System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=QeMhe7At-ho");
+            }
+        }
+    }
+}
