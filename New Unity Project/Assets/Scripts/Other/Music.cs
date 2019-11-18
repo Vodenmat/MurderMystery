6b8b1ed801c0e142274b5cf8fdd49d2ce7030eec
Timothy Landers <21.timothy.landers@ksd.org>
2019-11-18 09:04:42 -0800

    music

---
+++ New Unity Project/Assets/Scripts/Other/Music.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
